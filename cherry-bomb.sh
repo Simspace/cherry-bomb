8df30e85449555898b3fbe575dad2724d93cb949
shawn-bachlet <shawn.bachlet@simspace.com>
2022-03-22 16:10:03 -0400

    Add error handling

--- cherry-bomb.sh
+++ cherry-bomb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#bloop
 set -e
 
 while getopts 'r:c:' OPTION; do
@@ -15,6 +15,8 @@ while getopts 'r:c:' OPTION; do
     esac
 done
 
+failures=""
+
 if [ -n "${r}" ] &&  [ -n "${c}" ]
 then
   dir=$(mktemp -d)
@@ -24,10 +26,22 @@ then
   for branch_name in $(hub pr list -s open -f "%H "); do
     git checkout "${branch_name}"
     git cherry-pick "${c}"
-    git push
+    # This checks the status code of the previous command.
+    # git cherry-pick returns a 1 on conflict and a 0 on merge success
+    res=$(echo "$?")
+    if [ ${res} -eq 1 ]
+    then
+      git cherry-pick --abort
+      failures="${failures}${branch_name}\n"
+    else
+      git push
+    fi
     git checkout dev
     git reset --hard origin/dev
   done
+  failures="These branches failed to cherry-pick:\n"
+  printf ${failures}
+  rm -rf "${dir}"
 else
   echo "Usage: cherry-bomb [-c commit-hash]" >&2
 fi

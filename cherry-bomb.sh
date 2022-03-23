037c38b2c6f224191e4be32880e6f6bb5b6cf767
shawn-bachlet <shawn.bachlet@simspace.com>
2022-03-23 09:32:01 -0400

    Code review

--- cherry-bomb.sh
+++ cherry-bomb.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#bloop
 set -e
 
 while getopts 'r:c:' OPTION; do
@@ -20,27 +19,26 @@ failures=""
 if [ -n "${r}" ] &&  [ -n "${c}" ]
 then
   dir=$(mktemp -d)
+  trap 'rm -rf -- "$dir"' EXIT
   git clone "${r}" "${dir}"
   git reset --hard origin/dev
   cd "${dir}" || exit
   for branch_name in $(hub pr list -s open -f "%H "); do
     git checkout "${branch_name}"
-    git cherry-pick "${c}"
     # This checks the status code of the previous command.
     # git cherry-pick returns a 1 on conflict and a 0 on merge success
-    res=$(echo "$?")
-    if [ ${res} -eq 1 ]
+    if git cherry-pick "${c}";
     then
+      git push
+    else
       git cherry-pick --abort
       failures="${failures}${branch_name}\n"
-    else
-      git push
     fi
     git checkout dev
     git reset --hard origin/dev
   done
   failures="These branches failed to cherry-pick:\n"
-  printf ${failures}
+  printf '%s' "${failures}"
   rm -rf "${dir}"
 else
   echo "Usage: cherry-bomb [-c commit-hash]" >&2
